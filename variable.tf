4cbc29edd90dea1aea6ef51d73484441f9271e0c
Otumiky <otumiky@gmail.com>
2024-12-30 17:01:02 +0100

    t3-micro instance

--- variable.tf
+++ variable.tf
@@ -6,7 +6,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "The type of instance to use"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "db_password" {

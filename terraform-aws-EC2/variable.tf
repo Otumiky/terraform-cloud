0b0105d23efaf798e48cbcf530421babab323874
Otumiky <otumiky@gmail.com>
2024-12-30 23:52:21 +0100

    ec2 modules

---
+++ terraform-aws-EC2/variable.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+  description = "The Amazon Machine Image ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "db_password" {
+  description = "The database password"
+  type        = string
+  sensitive   = true
+}
+variable "aws_access_key_id" {
+  description = "access key id for aws"
+  type        = string
+}
+variable "aws_secret_access_key" {
+  description = "access key for aws"
+  type        = string
+}

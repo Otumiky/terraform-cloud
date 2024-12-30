23ae66b99242bb271e253175b1541bcc024fceae
Otumiky <otumiky@gmail.com>
2024-12-30 23:39:45 +0100

    ec2 module

---
+++ terraform-cloud-ec2-module-aws/main.tf
@@ -0,0 +1,17 @@
+
+provider "aws" {
+  region  = "us-east-1"  # Change to your desired region
+  access_key = var.aws_access_key_id  # Or specify directly if you don't want to use a variable
+  secret_key = var.aws_secret_access_key  # Same here
+}
+
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags = {
+    Name = "Terraform-Managed-Instance"
+  }
+}
+output "instance_id" {
+  value = aws_instance.app.id
+}

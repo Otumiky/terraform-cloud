074f1a525ff52aa85ef126c9951301d45cb389e2
Otumiky <otumiky@gmail.com>
2024-12-31 23:46:00 +0100

    ec2 module

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
-provider "aws" {
-  region  = "us-east-1"  # Change to your desired region
-
-}
-resource "aws_instance" "app" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name = "Terraform-Managed-Instance"
-  }
-}
-output "instance_id" {
-  value = aws_instance.app.id
+module "ec2_instance" {
+  source       = "app.terraform.io/massive-dynamic3/EC2/aws"
+  version      = "1.0.0"
+  ami_id       = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
 }

a8ac97aa039fa545ced163c90eedb1815c69063a
Otumiky <otumiky@gmail.com>
2024-12-31 01:43:46 +0100

    infra

--- main.tf
+++ main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source       = "app.terraform.io/massive-dynamic3/EC2/aws"
   version      = "1.0.0"
   ami_id       = "ami-0e2c8caa4b6378d8c"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 }

6eec531e0108a90a1582e47a71a11f3eb6be05be
Otumiky <otumiky@gmail.com>
2025-01-01 01:43:06 +0100

    updated t2-micro

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ module "ec2_instance" {
   source       = "app.terraform.io/massive-dynamic3/EC2/aws"
   version      = "1.0.1"
   ami_id       = "ami-0e2c8caa4b6378d8c"
-  instance_type = "t4g.micro"
+  instance_type = "t2.micro"
 }
